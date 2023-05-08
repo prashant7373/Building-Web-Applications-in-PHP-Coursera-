d781792e627105a89a1232beb4630ceb9ed2f198
prashant7373 <prashantgupta7373@gmail.com>
2023-05-09 01:11:23 +0530

    week 5

---
+++ week 5 php/index.php
@@ -0,0 +1,19 @@
+
+<title>Prashant Gupta PHP</title>
+<h1>Prashant Gupta PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Prashant Gupta\" is ".hash('sha256', 'Prashant Gupta');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
